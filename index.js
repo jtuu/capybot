47f559bcfa8eb90e7f81a431554e55b2654b19c4
jtuu <joona.tuukkanen@gmail.com>
2016-08-02 16:01:24 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const config = require("./config.default.json");
+
+//merge config file with defaults if exists
+try{
+  let configFile = require("./config.json");
+  Object.assign(config, configFile);
+}catch(err){
+  console.warn("Could not load config.json. Using default config.");
+}
+
+//a rudimentary REPL for debugging purposes
+function setupRepl(){
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", data => {
+    try{
+      console.log(eval(data));
+    }catch(err){
+      console.error(err);
+    }
+  });
+}
+setupRepl();
+
+//instantiate client
+const Client = require("./lib/client");
+const client = new Client(config);
+client.login();
