a3707a9d3aed79068d2f2fa899a5eaf0d5d07179
jtuu <joona.tuukkanen@gmail.com>
2016-08-04 16:40:15 +0300

    Created 8ball.js

---
+++ plugins/8ball.js
@@ -0,0 +1,36 @@
+var Plugin = require("../lib/plugin.js");
+
+var answers = [
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes, definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful"
+];
+var count = answers.length;
+var prefix = "8-ball says:";
+
+function randomAnswer() {
+	return answers[Math.floor(Math.random() * count)];
+}
+
+function payload(src, msg, type) {
+	this.sayToBoth(`${prefix} ${randomAnswer()}`);
+}
+
+module.exports = new Plugin("8ball", /^!8ball/, payload);
