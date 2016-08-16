360c1246c48283145f6c1498ec67ac67dfaaf621
jtuu <joona.tuukkanen@gmail.com>
2016-08-16 22:38:25 +0300

    Added moderate plugin Added more event hooks to relay

---
+++ plugins/moderate.js
@@ -0,0 +1,107 @@
+const Plugin = require("../lib/plugin.js");
+const steamPerms = require("steam").EChatPermission;
+const {
+	defaultReason
+} = require("../lib/utils").misc;
+
+const trigger = /^!(?:(k)(?:ick)?(b)(?:an)?|(k)(?:ick)?|(b)(?:an)?|(u)(?:n)?(b)(?:an)?)\b/;
+
+function payload(src, msg, type) {
+	let action = msg.match(trigger).slice(1).join("");
+	let [target, reason] = msg.replace(trigger, "").trim().split(/\s(.*$)/);
+
+	var actionCheck = () => {
+		if (action && target) {
+			switch (action) {
+				case "k":
+					kick.call(this, src, target, reason, type);
+					break;
+				case "b":
+					ban.call(this, src, target, reason, type);
+					break;
+				case "kb":
+					ban.call(this, src, target, reason, type);
+					kick.call(this, src, target, reason, type);
+					break;
+				case "ub":
+					unban.call(this, src, target, reason, type);
+					break;
+				default:
+					console.log("Unexpected input");
+					break;
+			}
+		}
+	}
+	if(type === "steam"){
+		this.irc.once("selfMessage", actionCheck);
+	}else{
+		actionCheck();
+	}
+}
+
+function kick(actor, target, reason = defaultReason, type) {
+	if (type === "steam") {
+		let {
+			permissions
+		} = this.steam.friends.chatRooms[this.room_id][actor];
+		if (permissions & steamPerms.Kick) {
+			this.irc.send("kick", this.channel, target, reason);
+		}
+	} else if (type === "irc") {
+		this.irc.send("names", this.channel);
+		this.irc.once("names" + this.channel, nicks => {
+			if (nicks[actor] === "@") {
+				Object.keys(this.steam.friends.personaStates)
+					.filter(id => this.steam.friends.personaStates[id].player_name === target)
+					.slice(0, 1)
+					.forEach(id => this.steam.friends.kick(this.room_id, id));
+			}
+		})
+	}
+}
+
+function ban(actor, target, reason = defaultReason, type) {
+	if (type === "steam") {
+		let {
+			permissions
+		} = this.steam.friends.chatRooms[this.room_id][actor];
+		if (permissions & steamPerms.Ban) {
+			this.irc.send("mode", this.channel, "+b", target);
+		}
+	} else if (type === "irc") {
+		this.irc.send("names", this.channel);
+		this.irc.once("names" + this.channel, nicks => {
+			if (nicks[actor] === "@") {
+				Object.keys(this.steam.friends.personaStates)
+					.filter(id => this.steam.friends.personaStates[id].player_name === target)
+					.slice(0, 1)
+					.forEach(id => this.steam.friends.ban(this.room_id, id));
+			}
+		})
+	}
+}
+
+function unban(actor, target, reason = defaultReason, type){
+	if(type === "steam"){
+		let {
+			permissions
+		} = this.steam.friends.chatRooms[this.room_id][actor];
+		if (permissions & steamPerms.Ban) {
+			this.irc.send("mode", this.channel, "-b", target);
+		}
+	}else if(type === "irc"){
+		this.irc.send("names", this.channel);
+		this.irc.once("names" + this.channel, nicks => {
+			if (nicks[actor] === "@") {
+				Object.keys(this.steam.friends.personaStates)
+					.filter(id => this.steam.friends.personaStates[id].player_name === target)
+					.slice(0, 1)
+					.forEach(id => this.steam.friends.unban(this.room_id, id));
+			}
+		})
+	}
+}
+
+const desc = "Kick or ban users.";
+
+module.exports = new Plugin("moderate", trigger, payload, desc, 5, true, false, false);
--- plugins/relay.js
+++ plugins/relay.js
@@ -1,6 +1,7 @@
 const Plugin = require("../lib/plugin.js");
 const {
-	defaultReason
+	defaultReason,
+	noopFn
 } = require("../lib/utils").misc;
 const Steam = require("steam");
 
@@ -31,22 +32,50 @@ function decorateNick(nick, color) {
 	return `<${nick}>`;
 }
 
+function formatUser(raw) {
+	return `${raw.nick} [${raw.user}@${raw.host}]`;
+}
+
 function colorNick(nick) {
 	let color = colors[nick.split("").reduce((p, c) => String(p.charCodeAt(0) + c.charCodeAt(0)), "\x00") % colors.length];
 	return colorChar + color + nick + endChar;
 }
 
-var relayOnJoin = function(nick) {
-	this.sayTo("steam", `${nick} has joined ${this.channel}`)
+var relayOnJoin = function(channel, nick, raw) {
+	this.sayTo("steam", `${formatUser(raw)} has joined ${channel}`)
 };
 
-var relayOnQuit = function(nick, reason = defaultReason) {
-	this.sayTo("steam", `${nick} has quit [${reason}]`)
+var relayOnQuit = function(nick, reason = defaultReason, channels, raw) {
+	this.sayTo("steam", `${formatUser(raw)} has quit [${reason}]`)
 };
 
-var relayOnPart = function(nick, reason = defaultReason) {
-	this.sayTo("steam", `${nick} has left ${this.channel} [${reason}]`)
+var relayOnPart = function(channel, nick, reason = defaultReason) {
+	this.sayTo("steam", `${formatUser(raw)} has left ${channel} [${reason}]`)
 };
+
+var relayOnKick = function(channel, nick, by, reason = defaultReason, raw) {
+	this.sayTo("steam", `${nick} was kicked from ${channel} by ${by} [${reason}]`)
+}
+
+var relayOnNick = function(oldNick, newNick, channels, raw) {
+	this.sayTo("steam", `${oldNick} is now known as ${newNick}`)
+}
+
+var relayOnMode = function(prefix, chan, by, mode, arg, raw) {
+	this.sayTo("steam", `mode/${chan} [${prefix}${mode}${arg ? " " + arg : ""}] by ${by}`)
+}
+
+var relayOnAction = function(from, to, text, raw) {
+	this.sayTo("steam", `* ${from} ${text}`);
+}
+
+var relayOnTopic = function(chan, topic, nick, raw) {
+	this.sayTo("steam", `${nick} changed the topic of ${chan} to: ${topic}`)
+}
+
+var relayOnKill = function(nick, reason = defaultReason, channels, raw){
+	this.sayTo("steam", `${nick} was killed [${reason}]`)
+}
 
 var relayOnChatStateChange = function(state, movingUser, room, actingUser) {
 	this.getSteamUser(movingUser).then(user => {
@@ -78,28 +107,44 @@ var relayOnChatStateChange = function(state, movingUser, room, actingUser) {
 	})
 };
 
-var noop = () => {};
-
 function initEvents() {
 	relay.init = true;
 
-	this.irc.removeListener("join" + this.channel, this.relayOnJoin || noop);
-	this.irc.removeListener("part" + this.channel, this.relayOnPart || noop);
-	this.irc.removeListener("quit", this.relayOnQuit || noop);
-	this.steam.friends.removeListener("chatStateChange", this.relayOnChatStateChange || noop);
+	this.irc.removeListener("join" + this.channel, this.relayOnJoin || noopFn);
+	this.irc.removeListener("part" + this.channel, this.relayOnPart || noopFn);
+	this.irc.removeListener("quit", this.relayOnQuit || noopFn);
+	this.irc.removeListener("nick", this.relayOnNick || noopFn);
+	this.irc.removeListener("+mode", this.relayOnModePlus || noopFn);
+	this.irc.removeListener("-mode", this.relayOnModeMinus || noopFn);
+	this.irc.removeListener("action", this.relayOnAction || noopFn);
+	this.irc.removeListener("topic", this.relayOnTopic || noopFn);
+	this.irc.removeListener("kick" + this.channel, this.relayOnKick || noopFn);
+	this.steam.friends.removeListener("chatStateChange", this.relayOnChatStateChange || noopFn);
 
-	this.relayOnJoin = relayOnJoin.bind(this);
-	this.relayOnPart = relayOnPart.bind(this);
+	this.relayOnJoin = relayOnJoin.bind(this, this.channel);
+	this.relayOnPart = relayOnPart.bind(this, this.channel);
 	this.relayOnQuit = relayOnQuit.bind(this);
+	this.relayOnNick = relayOnNick.bind(this);
+	this.relayOnModePlus = relayOnMode.bind(this, "+");
+	this.relayOnModeMinus = relayOnMode.bind(this, "-");
+	this.relayOnAction = relayOnAction.bind(this);
+	this.relayOnTopic = relayOnTopic.bind(this);
+	this.relayOnKick = relayOnKick.bind(this, this.channel);
 	this.relayOnChatStateChange = relayOnChatStateChange.bind(this);
 
 	this.irc.on("join" + this.channel, this.relayOnJoin);
 	this.irc.on("part" + this.channel, this.relayOnPart);
 	this.irc.on("quit", this.relayOnQuit);
+	this.irc.on("nick", this.relayOnNick);
+	this.irc.on("+mode", this.relayOnModePlus);
+	this.irc.on("-mode", this.relayOnModeMinus);
+	this.irc.on("action", this.relayOnAction);
+	this.irc.on("topic", this.relayOnTopic);
+	this.irc.on("kick" + this.channel, this.relayOnKick);
 	this.steam.friends.on("chatStateChange", this.relayOnChatStateChange);
 }
 
-const desc = "Relays messages and actions (joins, parts, quits, kicks, bans) between IRC and Steam.";
+const desc = "Relays messages and actions (joins, parts, quits, kicks, bans etc.) between IRC and Steam.";
 
 var relay = new Plugin("relay", "", payload, desc, 1, true, false, true);
 module.exports = relay;
