bb4016844e7423f0126bb21ac30df3725cacaf6b
jtuu <joona.tuukkanen@gmail.com>
2016-08-24 10:04:07 +0300

    Reworked cleverbot

--- plugins/cleverbot.js
+++ plugins/cleverbot.js
@@ -1,38 +1,21 @@
 const Plugin = require("../lib/plugin");
-const Cleverbot = require("cleverbot.io");
+const Cleverbot = require("cleverbot-node");
 
-var session = Promise.reject();
+const bot = new Cleverbot;
+var session = new Promise(res => Cleverbot.prepare(res));
 
 function payload(src, msg, type) {
   msg = msg.slice(4);
   return new Promise((resolve, reject) => {
-    if(!msg){
-      return reject(new Plugin.Response("no message"));
-    }
     session.then(() => {
-      bot.ask(msg, (err, response) => {
-        if(err)return reject(new Plugin.Response(err));
-
-        return resolve(new Plugin.Response(response));
+      bot.write(msg, response => {
+        resolve(new Plugin.Response(response["message"]));
       })
-    }).catch(err => {
-      reject(new Plugin.Response(err));
     })
   })
 }
 
 const desc = "Chat with Cleverbot.";
 const cbPlugin = new Plugin("cleverbot", /^!cb/, payload, desc, 5, true, false, true);
-
-cbPlugin.loadConfig();
-const bot = new Cleverbot(cbPlugin.config.user, cbPlugin.config.key);
-bot.setNick(cbPlugin.config.session);
-
-session = new Promise((res, rej) => {
-  bot.create((err, sess) => {
-    if(err)return rej(err);
-    res(sess);
-  })
-})
 
 module.exports = cbPlugin;
