e2bdec5dd12728015ed8c4b20ecf0905ceb6cee4
jtuu <joona.tuukkanen@gmail.com>
2016-11-04 10:56:18 +0200

    Created send.js

---
+++ plugins/send.js
@@ -0,0 +1,15 @@
+const Plugin = require("../lib/plugin.js");
+
+function payload(src, msg, type) {
+	const [_, to, thing] = msg.match(/^!send (\S+) (\S+)/) || [];
+
+	if (to && thing) {
+		return new Promise(resolve => {
+			resolve(new Plugin.Response(`Sending ${thing} to ${to}...`));
+		});
+	}
+}
+
+const desc = "Sent ;)";
+
+module.exports = new Plugin("send", /^!send/, payload, desc, 5, true, false, true);
